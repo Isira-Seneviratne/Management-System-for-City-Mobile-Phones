ef9f6200f59d9952392a0387d4adce0467527e28
Isira-Seneviratne <isirasen96@gmail.com>
2017-08-18 12:10:22 +0530

    Added SQL file and updated FS

---
+++ Management System.sql
@@ -0,0 +1,81 @@
+CREATE DATABASE ManagementSystem;
+
+CREATE TABLE Financial_Reports
+(
+	Month_issued	char(10),
+	Year_issued		int,
+	Total_revenue	float,
+	Total_costs		float,
+	Total_profit	float,
+	PRIMARY KEY(Month_issued, Year_issued)
+);
+
+CREATE TABLE Bill
+(
+	
+);
+
+CREATE TABLE Payment
+(
+	
+);
+
+CREATE TABLE Sold_Item
+(
+	
+);
+
+CREATE TABLE Warranty
+(
+	
+);
+
+CREATE TABLE Replace_Item
+(
+	
+);
+
+CREATE TABLE Repair_Item
+(
+	
+);
+
+CREATE TABLE Vendor
+(
+	
+);
+
+CREATE TABLE Distribution
+(
+	
+);
+
+CREATE TABLE Distribution_Item
+(
+	
+);
+
+CREATE TABLE Device
+(
+	
+);
+
+CREATE TABLE Device_Storage
+(
+	
+);
+
+CREATE TABLE Overtime
+(
+	
+);
+
+CREATE TABLE HR
+(
+	
+);
+
+CREATE TABLE Order_Details
+(
+
+);
