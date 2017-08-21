870823fdef180913761e7ca4e50da78393f0fb97
Wijesinghe M.B. it16043688 <it16043688@my.sliit.lk>
2017-08-21 08:36:24 +0530

    update sql

--- Management System.sql
+++ Management System.sql
@@ -44,17 +44,33 @@ CREATE TABLE Repair_Item
 
 CREATE TABLE Vendor
 (
+	Distrib_ID	varchar(10),
+	Manufac_ID	varchar(10),
+	Date		date,
+	Shipping_Cost	float,
+	PRIMARY KEY(Distrib_ID, Manufac_ID)
 	
 );
 
 CREATE TABLE Distribution
 (
-	
+	Vendor_ID	varchar(10),
+	Vendor_name	string,
+	Address		string,
+	Email		string,
+	Telephone	int,
+	Shipping_Cost_ph	float,
+	PRIMARY KEY(Vendor_ID)
+
 );
 
 CREATE TABLE Distribution_Item
 (
-	
+	Manufac_ID	varchar(10),
+	Item_ID		varchar(10),
+	Qty		int,
+	Item_Costs	int,
+	PRIMARY KEY(Manufac_ID, Item_ID)
 );
 
 CREATE TABLE Device
