424b4d5d619d5e1406117bf87ab3a195bd274eb2
Isira-Seneviratne <isirasen96@gmail.com>
2017-09-15 10:38:47 +0530

    Updated table structures

--- Management System.sql
+++ Management System.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE ManagementSystem;
+CREATE DATABASE IF NOT EXISTS ManagementSystem;
 
 USE ManagementSystem;
 
-CREATE TABLE Daily_Finances
+CREATE TABLE IF NOT EXISTS Daily_Finances
 (
         Day             int,
         Month           char(10),
@@ -24,7 +24,7 @@ CREATE TABLE Daily_Finances
         PRIMARY KEY(Day, Month, Year)
 );
 
-CREATE TABLE Financial_Reports
+CREATE TABLE IF NOT EXISTS Financial_Reports
 (
 	Month_issued	char(10),
 	Year_issued	int,
@@ -34,37 +34,28 @@ CREATE TABLE Financial_Reports
 	PRIMARY KEY(Month_issued, Year_issued)
 );
 
-CREATE TABLE Bill
+CREATE TABLE IF NOT EXISTS Bill
 (
 	
 );
 
-CREATE TABLE Payment
+CREATE TABLE IF NOT EXISTS Warranty
 (
 	
 );
 
-CREATE TABLE Sold_Item
+CREATE TABLE IF NOT EXISTS Replace_Item
+(
+	previous_item_code varchar(10) NOT NULL,
+        new_Item_code varchar(10) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Repair_Item
 (
 	
 );
 
-CREATE TABLE Warranty
-(
-	
-);
-
-CREATE TABLE Replace_Item
-(
-	
-);
-
-CREATE TABLE Repair_Item
-(
-	
-);
-
-CREATE TABLE Shipping_rec
+CREATE TABLE IF NOT EXISTS Shipping_rec
 (
 	Shipping_ID	varchar(10),
 	Vendor_ID	varchar(10),
@@ -75,7 +66,7 @@ CREATE TABLE Shipping_rec
 	PRIMARY KEY(Vendor_ID, Item_ID)
 );
 
-CREATE TABLE Vendor
+CREATE TABLE IF NOT EXISTS Vendor
 (
 	Vendor_ID	varchar(10),
 	Vendor_name	varchar(10),
@@ -86,7 +77,7 @@ CREATE TABLE Vendor
 	PRIMARY KEY(Vendor_ID)
 );
 
-CREATE TABLE Distribution_Item
+CREATE TABLE IF NOT EXISTS Distribution_Item
 (
 	Vendor_ID	varchar(10),
 	Item_ID		varchar(10),
@@ -95,27 +86,94 @@ CREATE TABLE Distribution_Item
 	PRIMARY KEY(Vendor_ID, Item_ID)
 );
 
-CREATE TABLE Device
+CREATE TABLE IF NOT EXISTS Device
 (
 	
 );
 
-CREATE TABLE Device_Storage
+CREATE TABLE IF NOT EXISTS Device_Storage
 (
 	
 );
 
-CREATE TABLE Overtime
+CREATE TABLE IF NOT EXISTS Overtime
 (
 	
 );
 
-CREATE TABLE HR
+CREATE TABLE IF NOT EXISTS HR
 (
 	
 );
 
-CREATE TABLE Order_Details
+CREATE TABLE IF NOT EXISTS Order_Details
 (
         
+);
+
+CREATE TABLE IF NOT EXISTS Item
+(
+        item_code varchar(10),
+        item_name varchar(50),
+        brand varchar(50),
+        manufacture_date date,
+        warranty_period int(11),
+        discount double,
+        selling_price double,
+        status varchar(20) DEFAULT 'not sold'
+);
+
+CREATE TABLE IF NOT EXISTS Payment
+(
+        customer_Id varchar(10),
+        loyalty_level varchar(20),
+        loyalty_discount double(10,0)
+);
+
+CREATE TABLE IF NOT EXISTS Vendor
+(
+        Vendor_ID varchar(10) DEFAULT '',
+        Vendor_name varchar(10),
+        Address varchar(10),
+        Email varchar(10),
+        Telephone int(11),
+        Shipping_Cost_ph float,
+        PRIMARY KEY (Vendor_ID)
+);
+
+CREATE TABLE IF NOT EXISTS Stock_item
+(
+        shippingID varchar(10) NOT NULL,
+        itemID int(11) NOT NULL,
+        itemName varchar(50) NOT NULL,
+        brand varchar(50) NOT NULL,
+        modelCode varchar(20) NOT NULL,
+        sellingPrice double NOT NULL,
+        manufactureDate varchar(50) NOT NULL,
+        warrantyPeriod int(11) NOT NULL,
+        PRIMARY KEY (shippingID,itemID)
+);
+
+CREATE TABLE IF NOT EXISTS Sold_item
+(
+        item_code varchar(10) NOT NULL,
+        bill_number varchar(10) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Loyalty customer
+(
+        customer_Id varchar(10) NOT NULL,
+        loyalty_level varchar(20) NOT NULL,
+        loyalty_discount double(10,0) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Reorder
+(
+        reOrderID int(11) NOT NULL,
+        vendorID int(11) NOT NULL,
+        modelCode varchar(50) NOT NULL,
+        Quantity int(11) NOT NULL,
+        totalCost double NOT NULL,
+        requestedDate date NOT NULL,
+        PRIMARY KEY (reOrderID)
 );
