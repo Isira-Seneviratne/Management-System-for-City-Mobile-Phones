afcaf7a7ae7848ab81e3e184fcae949bc5399351
Bhanu Wijesinghe <bhanukatha@gmail.com>
2017-09-23 11:54:45 +0530

    Small change

--- Insert Statements.sql
+++ Insert Statements.sql
@@ -20,10 +20,14 @@ INSERT INTO Phone_Repair (`repairID`, `itemName`, `modelCode`, `problem`, `EmpID
 (5, 'kdfkd', '', 'dcdcd', 0, '2 Sep 2017', 12, '2 Sep 2017'),
 (6, 'kdfkd', '', 'dcdcd', 0, '2 Sep 2017', 12, '2 Sep 2017');
 
-INSERT INTO `Daily_Finances` (`Day`,`Month`,`Year`,`Rep_inc`,`Rep_cost`,`Rep_prof`,`Sales_inc`,`Sales_cost`,`Sales_prof`,`Dis_inc`,`Dis_cost`,`Dis_prof`,`HR_cost`,`Other_cost`,`Tot_inc`,`Tot_cost`,`Tot_prof`) VALUES (19,'September',2017,300,300,0,300,300,300,0,0,0,300,50,600,950,-350),
+INSERT INTO `Daily_Finances`
+(`Day`,`Month`,`Year`,`Rep_inc`,`Rep_cost`,`Rep_prof`,`Sales_inc`,`Sales_cost`,`Sales_prof`,`Dis_inc`,`Dis_cost`,`Dis_prof`,`HR_cost`,`Other_cost`,`Tot_inc`,`Tot_cost`,`Tot_prof`) VALUES
+(19,'September',2017,300,300,0,300,300,300,0,0,0,300,50,600,950,-350),
 (22,'September',2017,300,300,0,0,0,0,0,0,0,300,50,300,650,-350);
 
 INSERT INTO `Financial_Reports`
 (`Month_issued`,`Year_issued`,`Total_revenue`,`Total_costs`,`Total_profit`) VALUES
 ('August',2017,400,60,340),
 ('September',2017,900,1500,-600);
+
+
